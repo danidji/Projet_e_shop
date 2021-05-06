105a01db2773bb201af1e1138c541a571c620962
dany gbelidji <dany.gbelidji@live.fr>
2021-05-06 14:32:01 +0200

    MOD - app. js - define road

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.sass';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+//import pages 
+import Home from './pages/Home'
+import Basket from './pages/Basket'
+import Product from './pages/Product'
+
+//import composants
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
 
+      <Navbar />
+      <Switch>
+
+        <Route exact path="/">
+          <Home />
         </Route>
+
+        <Route exact path="/panier">
+          <Basket />
+        </Route>
+
+        <Route exact path="/produit/:id">
+          <Product />
+        </Route>
+
+
       </Switch>
     </BrowserRouter>
   );

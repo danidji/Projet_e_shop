56d96b7a0398f70168336ddcb50725e2d7f44c1b
dany gbelidji <dany.gbelidji@live.fr>
2021-05-06 19:44:47 +0200

    🚧 WIP - style component button add by page location

--- src/components/ButtonAddBasket.jsx
+++ src/components/ButtonAddBasket.jsx
@@ -1,4 +1,10 @@
+import { useLocation } from 'react-router-dom';
+
+
+
 export default function ButtonAddBasket(props) {
+    const l = useLocation();
+
     return (
         <>
             <button className="button_add_basket" /* onClick={'#'} */>Ajouter</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 import { productDatabase } from '../lib/database'
 import ProductList from '../components/ProductList';
-// import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 
 
 
 export default function Home(props) {
+    const l = useLocation();
+    console.log(`Home -> l`, l)
 
 
     return (

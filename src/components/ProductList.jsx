253bc15759fbe0abc7fe9467605e8edb2a446338
dany gbelidji <dany.gbelidji@live.fr>
2021-05-07 17:01:12 +0200

    ✏️ Fix random typos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,7 @@
 import ProductCard from '../components/ProductCard';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
 
 
 export default function ProductList(props) {
-
-
-    let h = useHistory();
-    console.log(`h`, h)
-    let l = useLocation();
-    console.log(`l`, l)
-    let p = useParams();
-    console.log(`p`, p)
 
 
     return (
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -5,23 +5,19 @@ import ButtonAddBasket from '../components/ButtonAddBasket';
 
 export default function Product(props) {
     const p = useParams();
-    // console.log(`p`, p);
     const l = useLocation();
-    console.log(`Product -> l`, l)
 
     let myProduct = findProduct(p.id);
-    // console.log(`Product -> myProduct`, myProduct)
+
 
 
     return (
         <div className="product_page">
-            <div className="header">
-                < h1 >{myProduct.description}</h1 >
 
-            </div>
             <div className="content">
 
                 <img src={myProduct.urlImage} alt="" />
+                < h2 >{myProduct.description}</h2 >
                 <div className="price">{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(myProduct.unitPrice)}</div>
                 <ButtonAddBasket />
             </div>

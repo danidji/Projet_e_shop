ee900033df097b069fb25cd05ceb68ab8d84193c
dany gbelidji <dany.gbelidji@live.fr>
2021-05-10 12:54:17 +0200

    ✨ ADD - set quantity component

---
+++ src/components/SetQuantity.jsx
@@ -0,0 +1,21 @@
+import { AppContext } from '../AppContext';
+import { useContext } from 'react';
+
+
+export default function SetQuantity(props) {
+    const context = useContext(AppContext);
+
+    const handleclick = (productCode, qty, e) => {
+        // e.preventDefault();
+        context.setQuantityBasket(productCode, qty, e)
+    }
+
+    return (
+        <div className="product_quantity">
+            <button className="quantity less" onClick={(e) => handleclick(props.id, props.qty, e)}>-</button>
+            <div>{props.qty}</div>
+            <button className="quantity more" onClick={(e) => handleclick(props.id, props.qty, e)}>+</button>
+        </div>
+    )
+
+}
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 import { findProduct } from '../lib/database';
 import ButtonAddBasket from '../components/ButtonAddBasket';
+import SetQuantity from '../components/SetQuantity';
 
 export default function Product(props) {
     const p = useParams();
@@ -18,6 +19,7 @@ export default function Product(props) {
 
                 <img src={myProduct.urlImage} alt="" />
                 < h2 >{myProduct.description}</h2 >
+                <SetQuantity id={myProduct.id} qty={1} />
                 <div className="price">{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(myProduct.unitPrice)}</div>
                 <ButtonAddBasket />
             </div>

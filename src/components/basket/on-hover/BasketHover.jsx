6afd1c98e241267e564d6a425b92c93cb902c59e
dany gbelidji <dany.gbelidji@live.fr>
2021-05-12 10:02:36 +0200

    🚚 MOV - directory file on hover

--- src/components/basket/on-hover/BasketHover.jsx
+++ src/components/basket/on-hover/BasketHover.jsx
@@ -1,4 +1,4 @@
-import { AppContext } from '../../AppContext';
+import { AppContext } from '../../../AppContext';
 import { useContext } from 'react';
 import BasketHoverCard from './BasketHoverCard';
 

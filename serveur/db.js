af680aed5e6deb5ab1ae9c28d3cd1eb6f97cc763
dany gbelidji <dany.gbelidji@live.fr>
2021-05-12 22:55:26 +0200

    🗃️ ADD - DB file on server

---
+++ serveur/db.js
@@ -0,0 +1,22 @@
+module.exports = db = {
+    productDatabase: [
+
+        { productCode: 'DBZ', description: 'Dragonball Z Kai - Saga de Boo', unitPrice: 29.90, urlImage: 'https://i.ibb.co/28CjVkt/dbz.jpg' /* '/images/products/dbz.jpg' */, stockQuantity: 20 },
+        { productCode: 'FMA', description: 'Full Metal Alchemist Brotherhood', unitPrice: 19.50, urlImage: 'https://i.ibb.co/5x65s2j/fma.jpg'/* /images/products/fma.jpg' */, stockQuantity: 20 },
+        { productCode: 'SKY', description: 'Skyfall', unitPrice: 22.50, urlImage: 'https://i.ibb.co/XLZn4bG/sky.jpg' /* '/images/products/sky.jpg' */, stockQuantity: 20 },
+        { productCode: 'OPM', description: 'One Punch Man', unitPrice: 25.70, urlImage: 'https://i.ibb.co/cTxSSDb/opm.jpg' /* '/images/products/opm.jpg' */, stockQuantity: 20 },
+        { productCode: 'SWT', description: 'Star Wars épisode V', unitPrice: 29.90, urlImage: 'https://i.ibb.co/BV9Zz9R/swt.jpg'/*  '/images/products/swt.jpg' */, stockQuantity: 20 }
+    ]
+    , voucherDatabase: [
+        { 'NOEL2020': 0.12 },
+        { 'ANNIVERSAIRE': 0.15 },
+        { 'SOLDES_ETE': 0.25 }
+    ],
+    findProduct(productCode) {
+        return this.productDatabase.find(element => element.productCode === productCode)
+    },
+    findVoucher(voucherCode) {
+        return this.voucherDatabase.find(element => element[voucherCode])
+    }
+
+}

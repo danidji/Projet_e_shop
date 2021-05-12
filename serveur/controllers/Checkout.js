44098fd1b84cd21231ba8e70c6ae8508ddd1bbfe
dany gbelidji <dany.gbelidji@live.fr>
2021-05-12 22:57:09 +0200

    🚧WIP - Checkout basket

--- serveur/controllers/Checkout.js
+++ serveur/controllers/Checkout.js
@@ -1,31 +1,83 @@
-import { productDatabase, findProduct } from '../../src/lib/database';
+const YOUR_DOMAIN = 'http://localhost:3000';
+const db = require('../db');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default class Checkout {
+
+
+module.exports = class Checkout {
     constructor(basket) {
         this.basket = basket;
+        console.log('class checkout')
+    }
+    async returnlineItems(basket) {
+        return await this.basket.map((element) => {
+            let product = db.findProduct(element.productCode);
+            return {
+                price_data: {
+                    currency: 'eur',
+                    product_data: {
+                        name: product.description,
+                        images: [product.urlImage],
+                    },
+                    unit_amount: product.unitPrice,
+                },
+                quantity: element.qty,
+            }
+        })
     }
 
     async createSession(req, res) {
 
-        // const session = await stripe.checkout.sessions.create({
-        //     payment_method_types: ['card'],
-        //     line_items: this.basket.map((element) => {
-        //         return {
-        //             price_data: {
-        //                 currency: 'eur',
-        //                 product_data: {
-        //                     name: 'Stubborn Attachments',
-        //                     images: ['https://i.imgur.com/EHyR2nP.png'],
-        //                 },
-        //                 unit_amount: 200000,
-        //             },
-        //             quantity: 1,
-        //         }
-        //     }),
-        //     mode: 'payment',
-        //     success_url: `${YOUR_DOMAIN}/success`,
-        //     cancel_url: `${YOUR_DOMAIN}/cancel`,
-        // });
-        // res.json({ id: session.id });
+        console.log('async function ');
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: await this.returnlineItems(this.basket)
+            // line_items: [
+            //     {
+            //         price_data: {
+            //             currency: 'eur',
+            //             product_data: {
+            //                 name: 'Stubborn Attachments',
+            //                 images: ['https://i.imgur.com/EHyR2nP.png'],
+            //             },
+            //             unit_amount: 200000,
+            //         },
+            //         quantity: 1,
+            //     },
+            //     {
+            //         price_data: {
+            //             currency: 'eur',
+            //             product_data: {
+            //                 name: 'Stubborn Attachments',
+            //                 images: ['https://i.imgur.com/EHyR2nP.png'],
+            //             },
+            //             unit_amount: 200,
+            //         },
+            //         quantity: 1,
+            //     },
+            //     {
+            //         price_data: {
+            //             currency: 'eur',
+            //             product_data: {
+            //                 name: 'Stubborn Attachments',
+            //                 images: ['https://i.imgur.com/EHyR2nP.png'],
+            //             },
+            //             unit_amount: 20,
+            //         },
+            //         quantity: 1,
+            //     }
+            // ]
+
+
+
+
+
+            , mode: 'payment',
+            success_url: `${YOUR_DOMAIN}/success`,
+            cancel_url: `${YOUR_DOMAIN}/cancel`,
+        });
+        console.log(`Checkout -> createSession -> session`, session)
+
+        res.json({ id: session.id });
     }
 }
--- serveur/routes.js
+++ serveur/routes.js
@@ -1,58 +1,71 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const YOUR_DOMAIN = 'http://localhost:3000'
+const YOUR_DOMAIN = 'http://localhost:3000';
+const Checkout = require('./controllers/Checkout');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send("Salut mec bien")
-    })
-    app.post('/proceder-paiement', async (req, res) => {
+    // app.get('/', (req, res) => {
+    //     res.send("Salut mec bien")
+    // })
+    app.post('/proceder-paiement', (req, res) => {
         console.log(req.body.basket);
         let basket = req.body.basket;
-        let productDatabase = req.body.productDatabase
+        // let productDatabase = req.body.productDatabase
 
-        //création d'un session pour le paiement 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            line_items: [
-                {
-                    price_data: {
-                        currency: 'eur',
-                        product_data: {
-                            name: 'Stubborn Attachments',
-                            images: ['https://i.imgur.com/EHyR2nP.png'],
-                        },
-                        unit_amount: 200000,
-                    },
-                    quantity: 1,
-                },
-                {
-                    price_data: {
-                        currency: 'eur',
-                        product_data: {
-                            name: 'Stubborn Attachments',
-                            images: ['https://i.imgur.com/EHyR2nP.png'],
-                        },
-                        unit_amount: 200,
-                    },
-                    quantity: 1,
-                },
-                {
-                    price_data: {
-                        currency: 'eur',
-                        product_data: {
-                            name: 'Stubborn Attachments',
-                            images: ['https://i.imgur.com/EHyR2nP.png'],
-                        },
-                        unit_amount: 20,
-                    },
-                    quantity: 1,
-                }
-            ],
-            mode: 'payment',
-            success_url: `${YOUR_DOMAIN}/success`,
-            cancel_url: `${YOUR_DOMAIN}/cancel`,
-        });
-        res.json({ id: session.id });
+        //création d'un session pour le paiement
+        let repo = new Checkout(basket);
+        repo.createSession(req, res);
+
+
+
+
+
+
+
+
+
+        // 
+        // const session = await stripe.checkout.sessions.create({
+        //     payment_method_types: ['card'],
+        //     line_items: [
+        //         {
+        //             price_data: {
+        //                 currency: 'eur',
+        //                 product_data: {
+        //                     name: 'Stubborn Attachments',
+        //                     images: ['https://i.imgur.com/EHyR2nP.png'],
+        //                 },
+        //                 unit_amount: 200000,
+        //             },
+        //             quantity: 1,
+        //         },
+        //         {
+        //             price_data: {
+        //                 currency: 'eur',
+        //                 product_data: {
+        //                     name: 'Stubborn Attachments',
+        //                     images: ['https://i.imgur.com/EHyR2nP.png'],
+        //                 },
+        //                 unit_amount: 200,
+        //             },
+        //             quantity: 1,
+        //         },
+        //         {
+        //             price_data: {
+        //                 currency: 'eur',
+        //                 product_data: {
+        //                     name: 'Stubborn Attachments',
+        //                     images: ['https://i.imgur.com/EHyR2nP.png'],
+        //                 },
+        //                 unit_amount: 20,
+        //             },
+        //             quantity: 1,
+        //         }
+        //     ],
+        //     mode: 'payment',
+        //     success_url: `${YOUR_DOMAIN}/success`,
+        //     cancel_url: `${YOUR_DOMAIN}/cancel`,
+        // });
+        // res.json({ id: session.id });
 
 
 

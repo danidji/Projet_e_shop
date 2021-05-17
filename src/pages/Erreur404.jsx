f8573333b4bbba495aae46d04d1b2f477e535368
dany gbelidji <dany.gbelidji@live.fr>
2021-05-17 11:00:49 +0200

    🥅 ADD - Page 404 not found

---
+++ src/pages/Erreur404.jsx
@@ -0,0 +1,8 @@
+export default function Erreur404(props) {
+    return (
+        <div className="not_found">
+            <h1> Erreur 404 </h1>
+            <p> Cette page n'existe pas !!!</p>
+        </div>
+    )
+}

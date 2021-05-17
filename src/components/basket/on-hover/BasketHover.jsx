c033ce62c990d306a0937ba8418ea9a281282472
dany gbelidji <dany.gbelidji@live.fr>
2021-05-17 14:53:59 +0200

    🚧 WIP - Display basket on hover basket link

--- src/components/basket/on-hover/BasketHover.jsx
+++ src/components/basket/on-hover/BasketHover.jsx
@@ -1,28 +1,28 @@
-// import { AppContext } from '../../../AppContext';
-// import { useContext } from 'react';
-// import BasketHoverCard from './BasketHoverCard';
+import { AppContext } from '../../../AppContext';
+import { useContext } from 'react';
+import BasketHoverCard from './BasketHoverCard';
 
-// export default function BasketHover(props) {
-//     // console.log(`BasketHover -> props`, props)
-//     const context = useContext(AppContext);
-//     let basket = context.basket;
-//     function getBasketList() {
-//         return (
-//             basket.map((element, i) => {
-//                 console.log(`basket.map -> element`, element)
+export default function BasketHover(props) {
+    // console.log(`BasketHover -> props`, props)
+    const context = useContext(AppContext);
+    let basket = context.basket;
+    function getBasketList() {
+        return (
+            basket.map((element, i) => {
+                console.log(`basket.map -> element`, element)
 
-//                 return <BasketHoverCard id={element} key={i} />
-//             })
-//         )
+                return <BasketHoverCard id={element} key={i} />
+            })
+        )
 
-//     }
+    }
 
-//     return (
-//         <div className="basket_hover" onMouseLeave={() => props.onMouseLeave()}>
-//             <h4>Panier</h4>
-//             <div className="basket_hover_list">
-//                 {getBasketList()}
-//             </div>
-//         </div>
-//     )
-// }
+    return (
+        <div className="basket_hover" onMouseLeave={() => props.onMouseLeave()}>
+            <h4>Panier</h4>
+            <div className="basket_hover_list">
+                {getBasketList()}
+            </div>
+        </div>
+    )
+}
